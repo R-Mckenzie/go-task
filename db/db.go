a2ec24422b93272e812dd48f8f57d842e7ee691f
Ross Mckenzie <rossmckenzie11@gmail.com>
2022-04-22 17:05:31 +0100

    open DB

--- db/db.go
+++ db/db.go
@@ -1,3 +1,40 @@
 package db
 
+import (
+	"log"
+	"os"
+	"time"
 
+	"github.com/boltdb/bolt"
+)
+
+type Task struct {
+	title string
+	desc  string
+	start time.Time
+	end   time.Time
+}
+
+const databasePath = "/.cache/gotask/tasks.db"
+
+func openDb() bolt.DB {
+	dbLoc, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbLoc += databasePath
+
+	db, err := bolt.Open(dbLoc, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+func LoadTasks() ([]Task, err) {
+
+}
+
+func SaveTask(task Task) err {
+
+}
--- db/db_test.go
+++ db/db_test.go
@@ -3,5 +3,11 @@ package db_test
 import "testing"
 
 func TestDB(t *testing.T) {
+	t.Run("Can load tasks", func(t *testing.T) {
 
+	})
+
+	t.Run("can save tasks", func(t *testing.T) {
+
+	})
 }
