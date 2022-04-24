566d6c53425536a324e4088b1a35a9739cc2dc78
Ross Mckenzie <rossmckenzie11@gmail.com>
2022-04-24 16:20:33 +0100

    add tasks with description

--- cmd/add.go
+++ cmd/add.go
@@ -1,27 +1,22 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
 	"fmt"
 
+	"github.com/R-Mckenzie/gotask-cli/db"
 	"github.com/spf13/cobra"
 )
 
 // addCmd represents the add command
 var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "add \"task title\" \"task desctiption\"",
+	Short: "add a task to the task list",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		task := db.Task{Title: args[0], Desc: args[1]}
+		// validation
+		db.Save(task)
+		fmt.Printf("saved task %q\n", task.Title)
 	},
 }
 
